9b9e17c849b13155a75447137d124dd357bb2976
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-02-20 12:00:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn=document.querySelector("input");
+let select=document.querySelector("#colorSelect");
+
+btn.addEventListener("click" ,function(){
+
+	let newsel=select.options[select.selectedIndex];
+	  newsel.remove();
+})
